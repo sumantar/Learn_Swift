46877b8ed380ad2fc02e322a46e5a8e6cadd9050
sumanta rout <sumantar@cybage.com>
2014-10-31 11:28:09 +0530

    Modified

---
+++ PPT/Learn_Swift-3.playground/section-1.swift
@@ -0,0 +1,531 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+import Swift
+
+var str = "Hello, playground"
+
+let x = "Hi"
+/********************************************
+Subscripts
+*********************************************/
+
+/*
+Classes, structures, and enumerations can define subscripts, which are shortcuts for accessing the member elements of a collection, list, or sequence
+
+Syntax:
+subscript(index: Int) -> Int {
+    get {
+    // return an appropriate subscript value here
+    }
+    set(newValue) {
+    // perform a suitable setting action here
+    }
+}
+
+*/
+//Similar to computed property
+//For this exa,ple it doesnot make sense to have a setter for it.
+struct TimeTable{
+    let multiplier: Int
+    subscript(index: Int) -> Int{
+        get{
+            return multiplier * index
+        }
+    }
+}
+
+let table = TimeTable(multiplier: 3)
+println("Six time 3 is : \(table[6])");
+
+/*
+Usage: Swift’s Dictionary type implements a subscript to set and retrieve the values stored in a Dictionary instance
+
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
+
+*/
+var numberOfLegs = ["spider": 8, "ant": 6, "cat": 4]
+numberOfLegs["bird"] = 2
+
+/*
+Subscripts can take any number of input parameters, and these input parameters can be of any type
+
+Sucript can aso returns any type
+
+*/
+
+struct Matrix {
+    let rows: Int, columns: Int
+    var grid: [Double]
+    
+    init(rows: Int, columns: Int) {
+        self.rows = rows
+        self.columns = columns
+        grid = Array(count: rows * columns, repeatedValue: 0.0)
+    }
+    
+    func indexIsValidForRow(row: Int, column: Int) -> Bool {
+        return row >= 0 && row < rows && column >= 0 && column < columns
+    }
+    
+    subscript(row: Int, column: Int) -> Double {
+        get {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            return grid[(row * columns) + column]
+        }
+        set {
+            assert(indexIsValidForRow(row, column: column), "Index out of range")
+            grid[(row * columns) + column] = newValue
+        }
+    }
+   
+}
+
+var matrix = Matrix(rows: 2, columns: 2)
+matrix[0, 1] = 1.5
+
+matrix[1, 0] = 3.2
+
+extension String {
+    subscript(pattern: String) -> Bool {
+        get {
+            let range = self.rangeOfString(pattern)
+            
+            return (range?.isEmpty != nil)
+        }
+    }
+}
+
+var aString = "We ❤ Swift"
+aString["❤"] // true
+aString["Hello World"] // false
+
+/********************************************
+Protocols and Generics
+*********************************************/
+
+//Protocol and generic
+func findStringIndex(array: [String], valueToFind:String) -> Int?{
+    for (index, value) in enumerate(array){
+        if value == valueToFind{
+            return index
+        }
+    }
+    
+    return nil
+}
+
+func findStringIndexGeneric<T: Equatable>(array: [T], valueToFind:T) -> Int?{
+    for (index, value) in enumerate(array){
+        if value == valueToFind{
+            return index
+        }
+    }
+    
+    return nil
+}
+
+let doubleIndex = findStringIndexGeneric([3.14159, 0.1, 0.25], 9.3)
+let stringIndex = findStringIndexGeneric(["Mike", "Malcolm", "Andrea"], "Andrea")
+
+
+/********************************************
+Associated Types in protocol
+*********************************************/
+//Associated Types in protocol
+//We can define generics in protocol
+
+protocol Container{
+    typealias ItemType //This is not required as Swift can automatic type inference
+    mutating func append(item: ItemType)
+    var count :Int{get}
+    subscript(i: Int) -> ItemType {get}
+    
+}
+
+//Lets define a Integer stack that should implement this Container
+struct IntStack: Container{
+    var items = [Int]()
+    
+    mutating func push (item: Int){
+        items.append(item)
+        
+    }
+    
+    mutating func pop() -> Int{
+        return items.removeLast()
+    }
+    
+    //MARK: Protocol implementation
+    /*
+    typealias ItemType = Int
+    mutating func append(item: ItemType){
+        self.push(item)
+    }
+    */
+    mutating func append(item: Int){ //Automatic type inference
+        self.push(item)
+    }
+    
+    var count:Int{
+        return items.count
+    }
+    
+    subscript(i: Int) -> Int {
+        return items[i]
+    }
+    
+}
+
+var MyIntStack = IntStack()
+
+MyIntStack.push(5)
+MyIntStack.append(10)
+MyIntStack.append(20)
+MyIntStack.append(30)
+
+let value = MyIntStack[1]
+
+let value1 = MyIntStack[3]
+
+//Let have the generic implementation
+struct Stack<T>: Container{
+    var items = [T]()
+    
+    mutating func push(item: T){
+        items.append(item)
+    }
+    mutating func pop() -> T{
+        return items.removeLast()
+    }
+    
+    //MARK: protocol implementation
+    mutating func append(item: T){ //Automatic type inference
+        self.push(item)
+    }
+    
+    var count:Int{
+        return items.count
+    }
+    
+    subscript(i: Int) -> T {
+        return items[i]
+    }
+}
+
+//Protocol Constraints
+
+func allItemsMatch<T1: Container, T2: Container where T1.ItemType == T2.ItemType, T1.ItemType: Equatable> (someContainer: T1, anotherContainer: T2) -> Bool {
+    
+    // check that both containers contain the same number of items
+    if someContainer.count != anotherContainer.count {
+        return false
+    }
+    // check each pair of items to see if they are equivalent
+    for i in 0..<someContainer.count {
+        if someContainer[i] != anotherContainer[i] {
+            return false
+        }
+    }
+    // all items match, so return true
+    return true
+}
+
+
+var stackOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+
+var arrayOfStrings = Stack<String>()
+stackOfStrings.push("uno")
+stackOfStrings.push("dos")
+stackOfStrings.push("tres")
+stackOfStrings.push("tres1")
+
+if allItemsMatch(stackOfStrings, arrayOfStrings) {
+    println("All items match.")
+    let res = "All items match."
+} else {
+    println("Not all items match.")
+    let res = "Not All items match."
+}
+
+//Other protocols
+/********************************************
+Other System Level Protocols
+*********************************************/
+//Iterations - Sequence protocol
+
+class MyItem {
+    let name:String = ""
+    let priority:Int = 0
+    
+    init (name: String, priority: Int){
+        self.name = name
+        self.priority = priority
+    }
+}
+
+let item1 = MyItem(name: "abc", priority: 1)
+let item2 = MyItem(name: "def", priority: 2)
+
+//Lets have our collection class
+class Items : SequenceType{
+    var items: [MyItem] = []
+    func addItem(item: MyItem){
+        items.append(item)
+    }
+    
+    //Now we need to implement generate method
+    func generate()-> GeneratorOf<MyItem> {
+        var i = 0
+        return GeneratorOf<MyItem> {
+            return i >= self.items.count ? .None : self.items[i++]
+            //.None => nil i.e. end of the iterator
+        }
+    }
+
+}
+
+//Add items to collection class
+let items = Items()
+items.addItem(item1)
+items.addItem(item2)
+
+//Lets iterate
+
+for item in items{
+    println("item: \(item.name), \(item.priority)")
+}
+
+//Error: Items does not confirm to protocol SequenceType. Lets confirm to the protocol
+//Next Error: items does not confirm to protocol _Sequence_Type. Lets add method, GeneratorOf
+
+//Lets have a generic implementation
+struct GenericGenerator<T>: GeneratorType {
+    var items: [T]
+    mutating func next() -> T? {
+        return items.isEmpty ? .None : items.removeAtIndex(0)
+    }
+}
+
+////Generic Collection Class
+class ModifiedItems: SequenceType{
+    var collectonItems: [MyItem] = []
+    
+    func addItem(item: MyItem){
+        collectonItems.append(item)
+    }
+    
+    //Now we need to implement generate method
+    func generate() -> GenericGenerator<MyItem> {
+        return GenericGenerator(items: collectonItems)
+    }
+}
+
+let modifiedItems = ModifiedItems()
+modifiedItems.addItem(item1)
+modifiedItems.addItem(item2)
+//Now iterate
+for item in modifiedItems{
+    println("item: \(item.name) \(item.priority)")
+}
+
+
+//Boolean
+//Lets discuss our model class
+enum MyBool{
+    case MyTrue, MyFalse
+}
+
+extension MyBool{
+    init(){
+        self = .MyFalse
+    }
+}
+
+//Override == operator
+func == (lhs: MyBool, rhs: MyBool) -> Bool{
+    switch(lhs,rhs){
+    case (.MyTrue, .MyTrue), (.MyFalse, .MyFalse):
+        return true
+    default:
+        return false
+    }
+}
+
+extension MyBool: BooleanLiteralConvertible{
+    init(booleanLiteral value: Bool){
+        self = value ? MyTrue : MyFalse
+    }
+}
+
+extension MyBool : BooleanType{
+    var boolValue: Bool{
+        switch self{
+        case .MyTrue: return true
+        case .MyFalse: return false
+        }
+    }
+}
+//Lets assign a premitive type to our boolean type
+let a : MyBool = true
+//Error: MyBool does not confirm to protocol 'BooleanLiteralConvertiable'
+
+if a{
+    println("TRUE")
+}
+
+//Error: MyBool does not confirm to protocol BooleanType
+
+
+var basicBool: Bool = true
+let b = MyBool(booleanLiteral: basicBool)
+
+
+
+if a == a{
+    println("Objects are equals")
+}
+
+//It is already confirms to protocol Equatable
+
+//Protocol Equatable
+struct MyStruct: Equatable{
+    var name = "Untitled"
+}
+
+func == (lhs: MyStruct, rhs: MyStruct) -> Bool {
+    return lhs.name == rhs.name
+}
+
+var var1 = MyStruct()
+var var2 = MyStruct()
+let firstcheck = var1 == var2
+
+
+//Similar for printable and comparable
+
+/********************************************
+Optional: nil coalescing operator
+*********************************************/
+
+//a??b. It is similar to ternary operator. If a present, then it will return value of a elaee value of b
+let defaultColorName = "red"
+var userDefinedColorName: String?   // defaults to nil”
+var colorNameToUse = userDefinedColorName ?? defaultColorName
+
+userDefinedColorName = "green"
+colorNameToUse = userDefinedColorName ?? defaultColorName
+
+
+/********************************************
+Auto Closure and Asserts
+*********************************************/
+//Automatically closures will be added to the expressions
+
+func f(pred: ()->Bool){
+    if pred(){
+        println("It is true")
+    }
+}
+
+
+f({2 > 1})
+
+f({2 < 1})
+
+//f(2 > 1)
+//Error: Cound not find overloaded operator >
+//For convenient, it will be better to use in this form.
+//Solution: autoclosure
+
+
+func f1(pred: @autoclosure()->Bool){
+    if pred(){
+        println("It is true")
+    }
+}
+
+f1(2 > 1)
+f1(2 < 1)
+f1(5 > 3)
+
+func someExpensiveComputation() -> Int{
+    return 100
+}
+
+//It takes a cloure that only returns a boolean value.
+f1 (someExpensiveComputation() > 1)
+
+
+//Lets discuss about asserts
+func simpleAssert(condition: @autoclosure () -> Bool, message: String){
+    if condition(){
+        println(message)
+    }
+}
+
+let testNumber = 6
+simpleAssert(testNumber % 2 == 0, "testNumber isn't an even number.")
+
+let testNumber1 = 5
+simpleAssert(testNumber1 % 2 == 0, "testNumber isn't an even number.")
+
+//Lets discuss about another function
+func assert(x: Bool){
+    #if DEBUG
+       println("This is a debug statement")
+    #endif
+        
+}
+//But this function will be called in release build. Hence will affect the performance.
+//We can avoid it by lazy evaluation by adding autoclosure
+
+func assert1(predicate: @autoclosure () -> Bool){
+    #if DEBUG
+        if !predicate(){
+            println("assertion failed at \(__FILE__):\(__LINE__)")
+            abort()
+        }
+        //println("This is a debug statement")
+    #endif
+    
+}
+
+//assert1(someExpensiveComputation() != 42 )
+assert1(testNumber != 42 )
+//
+//func assert(condition: @autoclosure () -> Bool, _ message: String = "",
+//    file: String = __FILE__, line: Int = __LINE__) {
+//        #if DEBUG
+//            if !condition() {
+//            println("assertion failed at \(file):\(line): \(message)")
+//            abort()
+//            }
+//        #endif
+//}
+//
+//func logAndAssert(condition: @autoclosure () -> Bool, _ message: StaticString = "",
+//    file: StaticString = __FILE__, line: UWord = __LINE__) {
+//        
+//      //  logMessage(message)
+//      //  assert(condition, message, file: file, line: line)
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ PPT/Learn_Swift-4.playground/section-1.swift
@@ -0,0 +1,443 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+//Singleton
+
+class Singleton1{
+    class var sharedInstance: Singleton1{
+        return _SingletonASharedInstance
+    }
+}
+
+private let _SingletonASharedInstance = Singleton1()
+
+
+
+
+class Singleton {
+    class var sharedInstance : Singleton {
+        struct Static {
+            static var onceToken : dispatch_once_t = 0
+            static var instance : Singleton? = nil
+        }
+        dispatch_once(&Static.onceToken) {
+            Static.instance = Singleton()
+        }
+        return Static.instance!
+    }
+}
+
+class x{
+    class var x:Int{
+        return 0
+    }
+    
+}
+
+
+/////////////////////////////////////////////////////
+
+
+var str = "Hello, playground"
+
+//Function for quare of a number
+func square(a: Float) -> Float{
+    return a * a
+}
+
+//Function for cube of a number
+func cube(a: Float) -> Float{
+    return a * a * a
+}
+
+//Lets write function to give average of cube and square numbers
+func averageSumOfSquares(a: Float, b: Float) -> Float{
+    return (square(a) + square(b))/2
+}
+
+
+func averageSumOfCube(a: Float, b: Float) -> Float{
+    return (cube(a) + cube(b))/2
+}
+
+//Here, we will see the difference between function - cube or square. 
+//What is the best way??
+//Closure
+
+func averageFunction(a: Float, b: Float, f: (Float -> Float)) -> Float{
+    return (f(a) + f(b))/2
+}
+
+averageFunction(4.0, 3.0, square)
+averageFunction(4.0, 3.0, cube)
+
+
+//Currying
+//Currying is when you break down a function that takes multiple arguments into a series of functions that take part of the arguments.
+
+func averageCurriedFunction(f: (Float -> Float))(a: Float, b: Float) ->Float{
+    
+    return (f(a)+f(b))/2
+}
+
+let squareFunction = averageCurriedFunction(square)
+let cubeFunction = averageCurriedFunction(cube)
+
+let sqAvg = squareFunction(a: 4.0, b: 3.0)
+let cubAvg = cubeFunction(a: 4.0, b: 3.0)
+
+/********************************/
+/* Higher Order Function ********/
+/********************************/
+
+//Map functions
+var moneyArray = [10, 20, 32, 45]
+var stringArray = [String]()
+for money in moneyArray{
+    stringArray.append("\(money)$")
+}
+
+//Is this the best??
+//Here is the another approch
+let moneyArray1 = moneyArray.map ({
+    money in "\(money)$"
+})
+
+let moneyArray2 = moneyArray.map({
+      "\($0)$"
+})
+
+var numberArray = [20, 30, 40, 50]
+//Lets multiply 4 in each number
+numberArray.map({
+    $0 * 4
+})
+
+//Revisit the closure
+numberArray.map({
+    (number: Int) -> Int in
+    let result = number * 4
+    return result
+})
+
+//Since Swift does the type inferer, we can reduce it as follows
+numberArray.map({
+    number in number * 4
+})
+
+//Filter
+//In our earlier money array lets find values greater than 30 and result also an array
+
+let filterArray = numberArray.filter({
+    $0 > 30
+})
+
+// Lets find values greater than 100, which does not exist
+let filterArray1 = numberArray.filter({
+    $0 > 100
+})
+
+//Reduce
+//We often call it as fold or aggregate
+
+//Lets add all the numbers in number array
+let initialValue = 0
+let aggregate = numberArray.reduce(initialValue, {
+    $0 + $1
+})
+
+//Let multiply each members
+let aggregate1 = numberArray.reduce(1, {
+    $0 * $1
+})
+
+//Use Case for Map-Reduce-Filter
+let words = ["Cat", "Chicken", "fish", "Dog",
+    "Mouse", "Guinea Pig", "monkey"]
+
+let words_repeat = ["Cat", "Chicken", "fish", "Dog",
+    "Mouse", "Guinea Pig", "monkey"]
+
+//1. Lets join each string of first arry with every string of second array
+let result1 = words.map({
+    (word) -> [String] in
+    return words_repeat.map({
+        word + "&" + $0
+    })
+})
+
+let result2 = words.map({
+    (word) in
+    words_repeat.map({
+        (repeat) in
+        word + "&" + repeat
+    })
+})
+
+//2. Lets join each element of first array with first element of second array
+let result3 = words.map({
+    (word) -> String in
+    var index = find(words, word)
+    var secondValue = words_repeat[index!]
+    
+    return word + "->" + secondValue
+})
+
+
+/********************************/
+/** Other Highorder Functions ***/
+/********************************/
+
+//1. abs(signedNumber)
+abs(-1) == 1
+abs(-42) == 42
+abs(42) == 42
+
+//2. contains(sequence, element): Returns true if a given sequence (such as an array) contains the specified element
+var languages = ["Swift", "Objective-C"]
+contains(languages, "Swift") == true
+contains(languages, "Java") == false
+contains([29, 85, 42, 96, 75], 42) == true
+
+//3. dropFirst(sequence): Returns a new sequence (such as an array) without the first element of the sequence.
+var oldLanguages = dropFirst(languages)
+equal(oldLanguages, ["Objective-C"]) == true
+
+//4. dropLast(sequence): Returns a new sequence (such as an array) without the last element of the sequence passed as argument to the function
+languages = ["Swift", "Objective-C"]
+var newLanguages = dropLast(languages)
+equal(newLanguages, ["Swift"]) == true
+
+//5. dump(object): Dumps the contents of an object to standard output.
+languages = ["Swift", "Objective-C"]
+dump(languages)
+
+//6. equal(sequence1, sequence2): Returns true if sequence1 and sequence2 contain the same elements.
+languages = ["Swift", "Objective-C"]
+equal(languages, ["Swift", "Objective-C"]) == true
+oldLanguages = dropFirst(languages)
+equal(oldLanguages, ["Objective-C"]) == true
+
+//7. filter(sequence, includeElementClosure): Returns a the elements from sequence that evaluate to true by includeElementClosure.
+for i in filter(1...100, { $0 % 10 == 0 }) {
+    // 10, 20, 30, ...
+    println(i)
+    assert(contains([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], i))
+}
+
+//8. find(sequence, element): Return the index of a specified element in the given sequence. Or nil if the element is not found in the sequence.
+languages = ["Swift", "Objective-C"]
+find(languages, "Objective-C") == 1
+find(languages, "Java") == nil
+find([29, 85, 42, 96, 75], 42) == 2
+
+//9. indices(sequence): Returns the indices (zero indexed) of the elements in the given sequence.
+equal(indices([29, 85, 42]), [0, 1, 2])
+for i in indices([29, 85, 42]) {
+    // 0, 1, 2
+    println(i)
+}
+
+//10. join(separator, sequence): Returns the elements of the supplied sequence separated by the given separator
+join(":", ["A", "B", "C"]) == "A:B:C"
+languages = ["Swift", "Objective-C"]
+join("/", languages) == "Swift/Objective-C"
+
+//11. map(sequence, transformClosure): Returns a new sequence with the transformClosure applied to all elements in the supplied sequence.
+equal(map(1...3, { $0 * 5 }), [5, 10, 15])
+for i in map(1...10, { $0 * 10 }) {
+    // 10, 20, 30, ...
+    println(i)
+    assert(contains([10, 20, 30, 40, 50, 60, 70, 80, 90, 100], i))
+}
+
+//12. max(comparable1, comparable2, etc.): Returns the largest of the arguments given to the function
+max(0, 1) == 1
+max(8, 2, 3) == 8
+
+//13. maxElement(sequence): Returns the largest element in a supplied sequence of comparable elements.
+maxElement(1...10) == 10
+languages = ["Swift", "Objective-C"]
+maxElement(languages) == "Swift"
+
+//14. minElements(sequence): Returns the smallest element in a supplied sequence of comparable elements.
+minElement(1...10) == 1
+languages = ["Swift", "Objective-C"]
+minElement(languages) == "Objective-C"
+
+//15. reduce(sequence, initial, combineClosure): Recursively reduce the elements in sequence into one value by running the combineClosure on them with starting value of initial.
+languages = ["Swift", "Objective-C"]
+reduce(languages, "", { $0 + $1 }) == "SwiftObjective-C"
+reduce([10, 20, 5], 1, { $0 * $1 }) == 1000
+
+//16. reverse(sequence): Returns the elements of the given sequence reversed.
+equal(reverse([1, 2, 3]), [3, 2, 1])
+for i in reverse([1, 2, 3]) {
+    // 3, 2, 1
+    println(i)
+}
+
+//17. startsWith(sequence1, sequence2): Return true if the starting elements sequence1 are equal to the of sequence2.
+startsWith("foobar", "foo") == true
+startsWith(10..<100, 10..<15) == true
+languages = ["Swift", "Objective-C"]
+startsWith(languages, ["Swift"]) == true
+
+
+
+/********************************/
+/***** Tail Recursion    ********/
+/********************************/
+
+//Recursion and tail recursion
+//Standard recursion:
+//Factorial
+func factorial (n: Int) -> Int{
+    if n == 1{
+        return 1
+    }
+    else{
+        return n * factorial(n-1)
+    }
+    
+}
+
+let factorialRes = factorial(4)
+
+//Tail recursion
+
+/*
+Tail Recursion
+Tail Call
+
+Traditional Recursion: Final calculation will be carried out after all recursive function call. Hence each function call, compiler needs to add  new stack frame to call stack. Compiler has to remember entry point of the each call.
+i.e.  we perform recursive calls first, and then we take the return value of the recursive call and calculate the result. In this manner, we don't get the result of the calculation until we have returned from every recursive call
+
+Here is an example:
+
+sum(5) = 1 + 2 + 3 + 4 + 5 = 15
+
+recsum(5)
+5 + recsum(4)
+5 + (4 + recsum(3))
+5 + (4 + (3 + recsum(2)))
+5 + (4 + (3 + (2 + recsum(1))))
+5 + (4 + (3 + (2 + 1)))
+15
+
+In tail recursion, we will do calculation first. Then we execute recursion call, passing the results of the current step to the next step. We don't need current current stack frame any more. We optimized and will avoid stack overflow
+Basically, the return value of any given recursive step is the same as the return value of the next recursive call.
+
+Example:
+def tailrecsum(x, running_total=0):
+if x == 0:
+return running_total
+else:
+return tailrecsum(x - 1, running_total + x)
+
+
+Recursion Vs Iteration
+Tail recursion is similar to iteration
+
+*/
+
+
+//With tail recursion, we will add additional parameter - aggregator or result
+//Lets modify the above function to understand tail recursion
+func factorialWithToc(n: Int, result: Int = 1) -> Int{
+    
+    if n == 1{
+        return result
+    }
+    else{
+        return factorialWithToc(n-1, result: n * result)
+    }
+}
+
+let factorialToc = factorialWithToc(4)
+
+//Lets have another example - Fibonaci series
+func fibonaci(n: Int) -> Int{
+    if n < 2{
+        return n
+    }
+    else{
+        return fibonaci(n-1) + fibonaci(n-2)
+    }
+}
+
+let fibRes = fibonaci(3)
+
+func fibonaciWithToc(n: Int, previous: Int = 0, result: Int = 1) -> Int{
+    if n < 2{
+        return result
+    }
+    else{
+        return fibonaciWithToc(n-1, previous: result, result: result + previous)
+    }
+    
+}
+
+let fibResWithToc = fibonaciWithToc(3)
+
+/********************************/
+/***** Swift Memoization ********/
+/********************************/
+
+//memoize in swift
+//Swift Memoization
+//Memoization is an optimisation technique where the return values of a function are cached to avoid repeating the same computation
+/*
+func memoize<T: Hashable, U>( fn: (T->U, T)->U ) -> T->U {
+    var cache = [T : U]()
+    var result: ((T)->U)!
+    result = { x in
+        if let q = cache[x] { return q }
+        let r = fn(result, x)
+        cache[x] = r
+        return r
+    }
+    
+    return result
+}
+
+let fibonacci: (Int -> Double) = memoize {
+    fibonacci, n in
+    return (n < 2) ? Double(n) : fibonacci(n - 1) + fibonacci(n - 2)
+}
+
+let ret = fibonacci(45) / fibonacci(44)
+println(ret)
+
+let factorial = memoize { factorial, x in x == 0 ? 1 : x * factorial(x - 1) }
+let res = factorial(3)
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
